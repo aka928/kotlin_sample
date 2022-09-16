026855c146f78b6402a3981f6fd35424cd511d80
Tatsuya Akazawa <tatsuya_akazawa@r.recruit.co.jp>
2022-09-16 15:31:43 +0900

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,48 @@
+data class User(val name: String, var id: Int) {
+    object A {
+        val b = "b"
+    }
+
+    companion object {
+        val c = "c"
+    }
+}
+
+fun main() {
+
+    var user = User("ぽち", 1)
+
+    // let
+    val a = user.name.let {
+        it?.length
+    }
+    println(a)
+
+    // run
+    val size: Int = mutableListOf("red", "green", "blue").run {
+        add("yellow")
+        add("black")
+        size
+    }
+    println(size)
+
+    // also
+    val numbers = mutableListOf("one", "two", "three")
+    numbers.also { println("$it") }.add("four")
+
+    // apply
+    val list = mutableListOf(1, 2).apply {
+        add(3)
+        add(4)
+    }
+    println(list)
+
+    // with
+    val s = with("hoge") { this.length }
+    println(s)
+
+    // object
+    println(User.A.b)
+    println(User.c)
+
+}
